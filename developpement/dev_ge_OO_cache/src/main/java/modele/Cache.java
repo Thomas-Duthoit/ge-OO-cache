f90660b988430e9584b34a411699bc554099ff80
ccYuncc <160000960+ccYuncc@users.noreply.github.com>
2025-12-22 21:12:11 +0100

    dev Classe Cache

--- developpement/dev_ge_OO_cache/src/main/java/modele/Cache.java
+++ developpement/dev_ge_OO_cache/src/main/java/modele/Cache.java
@@ -2,8 +2,17 @@ package modele;
 
 import jakarta.persistence.*;
 
+@Entity
+@Table(name = "Cache")
 public class Cache {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int numero;
+
+    @Column(
+            name = "descriptionTextuelle",
+            length = 100
+    )
     private String descriptionTextuelle;
     private String descriptionTechnique;
     private String rubriqueLibre;
