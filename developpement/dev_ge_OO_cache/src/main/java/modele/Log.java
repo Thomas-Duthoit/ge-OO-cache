d1883afb64dac33c52ee9d9bb6b5984e70f49e16
Zatomate <duthoit.thomas9@gmail.com>
2025-12-27 11:15:57 +0100

    doc Log

--- developpement/dev_ge_OO_cache/src/main/java/modele/Log.java
+++ developpement/dev_ge_OO_cache/src/main/java/modele/Log.java
@@ -4,33 +4,67 @@ import jakarta.persistence.*;
 
 import java.util.Date;
 
-
+/**
+ * classe Log
+ * Représente une visite pour un cache
+ */
 @Entity(name = "Log")
 @Table(
-        name = "LOG"
+        name = "Log"
 )
 public class Log {
+
+    /**
+     *              ATTRIBUTS Log
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID", unique = true)
+    @Column(name = "ID_Log", unique = true)
     private int id;  // "clé primaire" avec la contrainte d'unicité pour l'entité
 
-    @Column(name = "COMMENTAIRE", nullable = false)
+    @Column(name = "commentaire", nullable = false, length = 100)  // non nulle, max 100 caractères
     private String commentaire;
-    private boolean trouver;
-    private int note;
-    @Column(name = "DATE", nullable = false)
-    private Date date;
 
-    // image à faire avec une url selon le temps restant
+    private boolean trouver;  // l'utilisateur a trouvé ou non la cache
 
-    public Log() {
+    private int note;  // note de l'utilisateur pour la cache
+
+    @Column(name = "date", nullable = false)
+    private Date date;  // date du log
+
+    // TODO: image à faire avec une url selon le temps restant
+
+
+
+    /**
+     *              ASSOCIATIONS Log
+     */
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "fk_Utilisateur_Id")
+    private Utilisateur proprietaire;  // association "possède" entre Utilisateur et Log
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "fk_Cache_Id")
+    private Cache enregistrer;  // association "enregistrer" entre Cache et Log
+
+
+    /**
+     *              CONSTRUCTEURS Log
+     */
+    public Log() {  // par défaut
         commentaire = ""; // vide par défaut
         date = new Date();
     }
 
+    /**
+     * Constructeur par données pour le Log
+     * @param commentaire commentaire laissé par l'utilisateur pour le log
+     * @param trouver la cache a été trouvée
+     * @param note note de la cache
+     * @param date date du log
+     */
     public Log(String commentaire, boolean trouver, int note, Date date) {
-        this();
+        this();  // appel du constructeur par défaut pour l'initialisation des variables par défaut
 
         if (commentaire != null) {
             this.commentaire = commentaire;
@@ -44,16 +78,10 @@ public class Log {
         }
     }
 
-    // Associations
-    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-    @JoinColumn(name = "PROPRIETAIRE_ID")
-    private Utilisateur proprietaire;  // association "possède" entre Utilisateur et Log
-
-    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-    @JoinColumn(name = "fk_Cache_Id")
-    private Cache enregistrer;  // association "enregistrer" entre Cache et Log
-
-
+    /**
+     * TOSTRING Log
+     * @return la chaine de caractère représentant les informations du log
+     */
     @Override
     public String toString() {
         return "Log{" +
@@ -69,16 +97,36 @@ public class Log {
 
 
 
-    // méthodes pour les associations
+    /**
+     *              METHODES ASSOCIATIONS Log
+     *.
+     * (Utilisées pour gérer les associations entre plusieurs classes)
+     */
 
-    public boolean setProprietaire(Utilisateur u) {
-        if (u != null) {
-            this.proprietaire = u;
-            return u.ajouterLogPossede(this);
+    /**
+     * Méthode : setProprietaire
+     * ---------------------------------
+     * Définit l'utilisateur comme le propriétaire de ce log
+     *
+     * @param utilisateur l'utilisateur qui a réalisé le log actuel
+     * @return affectation réussie
+     */
+    public boolean setProprietaire(Utilisateur utilisateur) {
+        if (utilisateur != null) {
+            this.proprietaire = utilisateur;
+            return utilisateur.ajouterLogPossede(this);
         }
         return false;
     }
 
+    /**
+     * Méthode : setCache
+     * ---------------------------------
+     * Définit la cache concernée par ce log
+     *
+     * @param cache la cache concernée par le log
+     * @return affectation réussie
+     */
     public boolean setCache(Cache cache) {
         if (cache != null) {
             this.enregistrer = cache;
