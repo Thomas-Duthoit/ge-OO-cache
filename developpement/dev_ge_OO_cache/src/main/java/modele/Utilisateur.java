388d1f63135526f3c4b4a3014ed053213ea18759
Zatomate <duthoit.thomas9@gmail.com>
2025-12-27 10:42:08 +0100

    documenting Utilisateur

--- developpement/dev_ge_OO_cache/src/main/java/modele/Utilisateur.java
+++ developpement/dev_ge_OO_cache/src/main/java/modele/Utilisateur.java
@@ -10,39 +10,51 @@ import java.util.List;
 // TODO: utiliser les noms de colonnes du MDP
 
 
-
+/**
+ * classe Utilisateur
+ * Contient toutes les informations relatives à un compte
+ */
 @Entity(name = "Utilisateur")
 @Table(
-        name = "UTILISATEUR"
+        name = "Utilisateur"
 )
 public class Utilisateur {
 
+    /**
+     *              ATTRIBUTS Utilisateur
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID", unique = true)
+    @Column(name = "ID_Utilisateur", unique = true)
     private int id;  // "clé primaire" avec la contrainte d'unicité pour l'entité
 
-    @Column(name = "PSEUDO", nullable = false)  // non nulle
+    @Column(name = "PSEUDO", nullable = false, length = 20)  // non nulle, max 20 caractères
     private String pseudo;  // Pseudo d'un utilisateur
-    @Column(name = "MDP", nullable = false)  // non nulle
+
+    @Column(name = "MDP", nullable = false, length = 20)  // non nulle, max 20 caractères
     private String mdp;  // mot de passe de l'utilisateur
+
     private boolean admin;  // utilisateur administrateur ?
 
 
 
-    // Associations:
+    /**
+     *              ASSOCIATIONS Utilisateur
+     */
     @ManyToMany(mappedBy = "utilisateurs", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-    private List<ReseauCache> accede;
-
-
-    @OneToMany(mappedBy = "proprietaire", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-    private List<ReseauCache> possede;
+    private List<ReseauCache> accede;  // Association "accède" entre Utililsateur et ReseauCache
 
     @OneToMany(mappedBy = "proprietaire", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-    private List<Log> possedeLog;
+    private List<ReseauCache> possede;  // Association "possède" entre Utililsateur et ReseauCache
+
+    @OneToMany(mappedBy = "proprietaire", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private List<Log> possedeLog;  // Association "ppossède" entre Utililsateur et Log
 
 
-    public Utilisateur() {
+    /**
+     *              CONSTRUCTEURS Utilisateur
+     */
+    public Utilisateur() {  // Par défaut
         this.pseudo = "";
         this.mdp = "";
         this.accede = new ArrayList<>();
@@ -50,8 +62,14 @@ public class Utilisateur {
         this.possedeLog = new ArrayList<>();
     }
 
+    /**
+     * Constructeur par données pour l'Utilisateur
+     * @param pseudo pseudo de l'utilisateur
+     * @param mdp mot de passe de l'utilisateur
+     * @param admin true=admin, false=utilisateur simple
+     */
     public Utilisateur(String pseudo, String mdp, boolean admin) {
-        this();
+        this();  // appel du constructeur par défaut pour l'initialisation des variables
         if (pseudo != null) {
             this.pseudo = pseudo;
         }
@@ -61,6 +79,10 @@ public class Utilisateur {
         this.admin = admin;
     }
 
+    /**
+     * TOSTRING Utilisateur
+     * @return la chaine de caractère représentant les informations de l'utilisateur
+     */
     @Override
     public String toString() {
         return "Utilisateur{" +
@@ -75,23 +97,61 @@ public class Utilisateur {
     }
 
 
+    /**
+     *              METHODES ASSOCIATIONS Utilisateur
+     *
+     * (Utilisées pour gérer les associations entre plusieurs classes)
+     */
 
-    // méthodes pour les associations:
-    protected void ajouterAccesReseau(ReseauCache r) {
-        if (r != null) {
-            this.accede.add(r);
+    /**
+     * méthode : ajouterAccesReseau
+     * ----------------------------
+     * Ajoute l'accès au réseau pour l'utilisateur courant
+     * /!\ Comme l'utilisateur n'est pas propriétaire de l'association, on doit faire la manipulation depuis l'instance du
+     *     reseau et cette instance appelera cette méthode
+     *
+     * @param reseauCache le réseau à accéder
+     * @return affectation réussie ou non
+     */
+    protected boolean ajouterAccesReseau(ReseauCache reseauCache) {
+        if (reseauCache != null) {
+            this.accede.add(reseauCache);
+            return true;
         }
+        return false;
     }
 
-    protected void ajouterReseauPossede(ReseauCache r) {
-        if (r != null) {
-            this.possede.add(r);
+    /**
+     * méthode : ajouterReseauPossede
+     * ------------------------------
+     * Définit le réseau comme appartenant à l'utilisateur courant
+     * /!\ Comme l'utilisateur n'est pas propriétaire de l'association, on doit faire la manipulation depuis l'instance du
+     *     reseau et cette instance appelera cette méthode
+     *
+     * @param reseauCache le réseau qui nous appartient
+     * @return affectation réussie ou non
+     */
+    protected boolean ajouterReseauPossede(ReseauCache reseauCache) {
+        if (reseauCache != null) {
+            this.possede.add(reseauCache);
+            return true;
         }
+        return false;
     }
 
-    protected boolean ajouterLogPossede(Log l) {
-        if (l != null) {
-            this.possedeLog.add(l);
+    /**
+     * méthode : ajouterLogPossede
+     * ---------------------------
+     * Définit le log comme appartenant à l'utilisateur courant
+     * /!\ Comme l'utilisateur n'est pas propriétaire de l'association, on doit faire la manipulation depuis l'instance du
+     *     log et cette instance appelera cette méthode
+     *
+     * @param log le réseau qui nous appartient
+     * @return affectation réussie ou non
+     */
+    protected boolean ajouterLogPossede(Log log) {
+        if (log != null) {
+            this.possedeLog.add(log);
             return true;
         }
         return false;
